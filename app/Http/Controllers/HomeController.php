c1c1b4a9b1f346041d532aaade760c6a4d94552f
Nicole Camila Niño Ariza <ninocamila28@gmail.com>
2026-03-18 00:07:03 -0500

    Landing page del e-commerce

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('welcome');
+        $featuredProducts = Product::latest()->limit(4)->get();
+        $categories = Category::limit(8)->get();
+        
+        return view('welcome', [
+            'featuredProducts' => $featuredProducts,
+            'categories' => $categories,
+        ]);
     }
 }
